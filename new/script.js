5285db56c57add9ff8c0804ffcfb2d6744ff23a0
sammymallya <144169439+sammymallya@users.noreply.github.com>
2024-04-14 17:18:58 +0530

    committing

---
+++ new/script.js
@@ -0,0 +1,13 @@
+
+    document.addEventListener("DOMContentLoaded", function() {
+        //  the EXPLORE button element
+        var exploreButton = document.getElementById("btn");
+    
+        //  event listener to the EXPLORE button
+        exploreButton.addEventListener("click", function() {
+            // Redirect to another page when the button is clicked
+            window.location.href = "event1.html";
+        }
+    );
+});
+
